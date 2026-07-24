08d91af1d60d5359ca01515f3573b797691bb091
diego bucci <forest73@libero.it>
2026-07-24 17:29:56 +0200

    Delete frontend directory

--- frontend/src/data/site.js
+++
@@ -1,9 +0,0 @@
-export const SITE = {
-  artist: "Cortona Francesca",
-  email: "cortonaitalianpainter@gmail.com",
-  whatsapp: "393922731473",
-  whatsappDisplay: "+39 392 273 1473",
-  facebook: "https://www.facebook.com/CortonaFrancescaArcani",
-  instagram: "https://www.instagram.com/francescacortona?igsh=MTN6eXZ0dmo3YXI0eQ%3D%3D&utm_source=qr",
-  opensea: "https://opensea.io/collection/cortonaitalianpainter",
-};
